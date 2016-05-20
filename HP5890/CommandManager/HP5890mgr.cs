92a34e4ef639ad2a3484e08bd17a6facbb814f51
Dorel <lzrdig@outlook.com>
2016-05-19 22:02:32 -0500

    Added to the list of commands

--- HP5890/CommandManager/HP5890mgr.cs
+++ HP5890/CommandManager/HP5890mgr.cs
@@ -3,43 +3,43 @@ using System.IO.Ports;
 using System.Collections.Generic;
 using System.Windows.Forms;
 
-//Command Code    Description
+//Command     Code Description
 
-//ID    Identification command
-//EP    Enable Even Parity
+//ID Identification command
+//EP Enable Even Parity
 //OP Enable Odd Parity
 //ZP Disable Parity
-//DF      Set HP 5890 default configuration
-//E+		Enable echo
-//E-		Disable echo(default)
-//H+		Enable host handshake -ENQ/ACK
-//H-		Disable host handshake(default)
-//N+		Enable HP 5890 (node) handshake -ENQ/ACK(default)
-//N-		Disable HP 5890 (node) handshake
-//RS      Define read sequence(default (none))
+//DF Set HP 5890 default configuration
+//E+ Enable echo
+//E- Disable echo(default)
+//H+ Enable host handshake -ENQ/ACK
+//H- Disable host handshake(default)
+//N+ Enable HP 5890 (node) handshake -ENQ/ACK(default)
+//N- Disable HP 5890 (node) handshake
+//RS Define read sequence(default (none))
 //TS Define termination sequence(default (CR/LF))
-//X+		Enable XON/XOFF protocol
-//X-		Disable XON/XOFF protocol(default)
+//X+ Enable XON/XOFF protocol
+//X- Disable XON/XOFF protocol(default)
 //AR Acquire or release remote control; locks keyboard
 //AT read actual temperatures and flows
 //CA change all; send a workfile to HP 5890
 //EV Read HP 5890 environmental status
 //IC read hp 5890 instrument configuration
 //IF read hp 5890 instrument fault string
-//IK      read last hp 5890 key pressed
+//IK read last hp 5890 key pressed
 //LA list all; receive a workfile from HP 5890
-//LU		lock or unlock the start key
-//OT      output text to the hp 5890 display
-//RF      read Hp 5890 serial interface fault word
-//RI      read run and readiness information
-//RK      perform remote keystrokes from the host
+//LU lock or unlock the start key
+//OT output text to the hp 5890 display
+//RF read Hp 5890 serial interface fault word
+//RI read run and readiness information
+//RK perform remote keystrokes from the host
 //RT retransmit last message sent by the hp 5890
 //HR delays start until host is ready
-//SM      requests that the host be notified when the HP 5890 status is modified
-//ST      start notification when hp 5890 begins to run
-//CD      configure data transfer
-//CP      configure signal producers
-//SD      send signal data
+//SM requests that the host be notified when the HP 5890 status is modified
+//ST start notification when hp 5890 begins to run
+//CD configure data transfer
+//CP configure signal producers
+//SD send signal data
 
 namespace HP5890
 {
@@ -73,8 +73,9 @@ namespace HP5890
     class HP5890mgr
     {
         private Char _terminationChr = '\n';
-        private Char _commandChr;
+        private String _preCommand = "\x1B"+"C";
         private String _openPortName = "COM1";
+        private SerialPort _serialPort = null;
         protected Dictionary<string,HP5890command> gcHP5890cmds = new Dictionary<string,HP5890command>();
 
         /// <summary>
@@ -87,6 +88,7 @@ namespace HP5890
         {
             _openPortName = portName;
             InitCommandList();
+            _serialPort = new SerialPort(portName);
         }
 
         #region HP5890mgr Properties
@@ -96,10 +98,10 @@ namespace HP5890
             get { return _terminationChr; }
         }
 
-        public Char CmdChar
+        public String PreCmdChar
         {
-            set { _commandChr = value; }
-            get { return _commandChr; }
+            set { _preCommand = value; }
+            get { return _preCommand; }
         }
 
         public String OpenPortName
@@ -113,10 +115,28 @@ namespace HP5890
         {
             HP5890command cmdObj = new HP5890command();
 
-            cmdObj.Apply("ID", "Identification command", "Identification");
+            cmdObj.Apply("ID", "Identification Command", "Identification");
             gcHP5890cmds.Add("ID", cmdObj);
             cmdObj.Apply("EP", "Enable Even Parity", "Configuration");
             gcHP5890cmds.Add("EP", cmdObj);
+            cmdObj.Apply("OP", "Enable Odd Parity", "Configuration");
+            gcHP5890cmds.Add("OP", cmdObj);
+            cmdObj.Apply("ZP", "Disable Parity", "Configuration");
+            gcHP5890cmds.Add("ZP", cmdObj);
+            cmdObj.Apply("DF", "Set HP5890 Default Configuration", "Configuration");
+            gcHP5890cmds.Add("DF", cmdObj);
+            cmdObj.Apply("E+", "Enable Echo", "Configuration");
+            gcHP5890cmds.Add("E+", cmdObj);
+            cmdObj.Apply("E-", "Disable Echo", "Configuration");
+            gcHP5890cmds.Add("E-", cmdObj);
+            cmdObj.Apply("H+", "Enable Host Handshake -ENQ/ACK (default)", "Configuration");
+            gcHP5890cmds.Add("H+", cmdObj);
+            cmdObj.Apply("H-", "Disable Host Handshake", "Configuration");
+            gcHP5890cmds.Add("H-", cmdObj);
+            cmdObj.Apply("N+", "Enable HP5890 (node) Handshake -ENQ/ACK (default)", "Configuration");
+            gcHP5890cmds.Add("N+", cmdObj);
+            cmdObj.Apply("N-", "Disable HP5890 (node) Handshake -ENQ/ACK (default)", "Configuration");
+            gcHP5890cmds.Add("N+", cmdObj);
             cmdObj.Apply("LA", "List all settings", "Configuration");
             gcHP5890cmds.Add("LA", cmdObj);
         }
@@ -125,7 +145,7 @@ namespace HP5890
         {
             string commandToSend;
 
-            commandToSend = cmdStr + _terminationChr + _commandChr;
+            commandToSend = cmdStr + _terminationChr;
 
             try {
                 SerialPort curPort = new SerialPort(_openPortName);
